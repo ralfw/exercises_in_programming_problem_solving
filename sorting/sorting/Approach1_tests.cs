d883ec38d6851fdfe855fb7973c550774859d91b
Ralf Westphal <info@ralfw.de>
2019-01-01 18:11:47 +0100

    r deleted scaffolding tests

--- sorting/sorting/Approach1_tests.cs
+++ sorting/sorting/Approach1_tests.cs
@@ -7,64 +7,26 @@ namespace sorting
     {
                 
         [Fact]
-        public void Acceptance_test() {
+        public void Acceptance_test_case_1() {
             var values = new[] {3,10,7,-2,5,10,2,0,9,4};
             
             var result = Sort(values);
 
             var expected = new[] {-2, 0, 2, 3, 4, 5, 7, 9, 10, 10};
             Assert.Equal(expected, result);
+            Assert.NotSame(result, values);
         }
         
         
         [Fact]
-        public void Empty_array() {
+        public void Acceptance_test_case_2() {
             var values = new int[0];
             var result = Sort(values);
             Assert.Empty(result);
         }
         
-        [Fact]
-        public void Treat_input_as_immutable() {
-            var values = new[] {2};
-            var result = Sort(values);
-            Assert.NotSame(result, values);
-            Assert.Equal(new[]{2}, result);
-        }
         
-        
-        [Fact]
-        public void Swap_two_unsorted_values() {
-            var values = new[] {3,2};
-            var result = Sort(values);
-            Assert.Equal(new[]{2,3}, result);
-        }
-        
-        
-        [Fact]
-        public void Dont_swap_two_sorted_values() {
-            var values = new[] {2,3};
-            var result = Sort(values);
-            Assert.Equal(new[]{2,3}, result);
-        }
-       
-        
-        [Fact]
-        public void Repeat_swapping_with_single_pass() {
-            var values = new[] {3,1,2};
-            var result = Sort(values);
-            Assert.Equal(new[]{1,2,3}, result);
-        }
-        
-        
-        [Fact]
-        public void Repeat_swapping_with_multiple_passes() {
-            var values = new[] {3,2,1};
-            var result = Sort(values);
-            Assert.Equal(new[]{1,2,3}, result);
-        }
-        
-        
+
         
         private int[] Sort(int[] values) {
             var result = (int[])values.Clone();
