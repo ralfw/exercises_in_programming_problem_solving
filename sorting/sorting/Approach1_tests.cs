71094abefca15e811d8a28f65765e630205e9521
Ralf Westphal <info@ralfw.de>
2019-01-01 17:04:56 +0100

    Added red acceptance test (yeah, I now, it's a bit late ;-)

--- sorting/sorting/Approach1_tests.cs
+++ sorting/sorting/Approach1_tests.cs
@@ -5,6 +5,18 @@ namespace sorting
 {
     public class Approach1_tests
     {
+                
+        [Fact]
+        public void Acceptance_test() {
+            var values = new[] {3,10,7,-2,5,10,2,0,9,4};
+            
+            var result = Sort(values);
+
+            var expected = new[] {-2, 0, 2, 3, 4, 5, 7, 9, 10, 10};
+            Assert.Equal(expected, result);
+        }
+        
+        
         [Fact]
         public void Empty_array() {
             var values = new int[0];
