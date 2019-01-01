f75773492e913d4061ffe43a0df2b3460cc5aeed
Ralf Westphal <info@ralfw.de>
2019-01-01 17:01:02 +0100

    Red: treat input as immutable

--- sorting/sorting/Approach1_tests.cs
+++ sorting/sorting/Approach1_tests.cs
@@ -6,11 +6,18 @@ namespace sorting
     public class Approach1_tests
     {
         [Fact]
-        public void Empty_array()
-        {
+        public void Empty_array() {
             var values = new int[0];
             var result = Sort(values);
             Assert.Empty(result);
+        }
+        
+        [Fact]
+        public void Treat_input_as_immutable() {
+            var values = new[] {2};
+            var result = Sort(values);
+            Assert.NotSame(result, values);
+            Assert.Equal(new[]{2}, result);
         }
 
 
