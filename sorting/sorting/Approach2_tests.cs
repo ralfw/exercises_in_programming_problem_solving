3127102f424e45d895126d9de84c489cf175f5c0
Ralf Westphal <info@ralfw.de>
2019-01-02 16:31:29 +0100

    green - for array with value larger than pivot

--- sorting/sorting/Approach2_tests.cs
+++ sorting/sorting/Approach2_tests.cs
@@ -55,6 +55,7 @@ namespace sorting
         [Theory]
         [InlineData(new[]{4}, 4, 0, 1, 0)]
         [InlineData(new[]{4,4}, 4, 0, 2, 0)]
+        [InlineData(new[]{5,4}, 4, 0, 1, 1)]
         public void Partiton_tests(int[] values, int pivot, int expectedLessThanLen, int expectedEqualToLen, int expectedLargerThanLen)
         {
             var result = Partition(values, pivot);
