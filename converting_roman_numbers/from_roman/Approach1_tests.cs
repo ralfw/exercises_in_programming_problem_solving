0818a2378b875e55cb3e440f3947e201f920fd15
Ralf Westphal <info@ralfw.de>
2019-01-04 12:16:06 +0100

    Red-green: generalizing single digit conversion

--- converting_roman_numbers/from_roman/Approach1_tests.cs
+++ converting_roman_numbers/from_roman/Approach1_tests.cs
@@ -18,6 +18,11 @@ namespace from_roman
         [Theory]
         [InlineData("I", 1)]
         [InlineData("V", 5)]
+        [InlineData("X", 10)]
+        [InlineData("L", 50)]
+        [InlineData("C", 100)]
+        [InlineData("D", 500)]
+        [InlineData("M", 1000)]
         public void Convert_single_digit(string roman, int expectedDecimal)
         {
             var result = FromRoman(roman);
@@ -25,11 +30,17 @@ namespace from_roman
         }
 
         
-        private int FromRoman(string roman)
-        {
-            if (roman == "I") return 1;
-            if (roman == "V") return 5;
-            throw new InvalidOperationException();
+        private int FromRoman(string roman) {
+            switch (roman) {
+                case "I": return 1;
+                case "V" : return 5;
+                case "X": return 10;
+                case "L": return 50;
+                case "C": return 100;
+                case "D": return 500;
+                case "M": return 1000;
+                default: throw new InvalidOperationException();
+            }
         }
     }
 }
