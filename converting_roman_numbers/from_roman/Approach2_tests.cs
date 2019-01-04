456c0bb483f834e120877f97cd3b22de4172322b
Ralf Westphal <info@ralfw.de>
2019-01-04 13:01:16 +0100

    Red-green: Second increment for parsing. Generalization of digit conversion.

--- converting_roman_numbers/from_roman/Approach2_tests.cs
+++ converting_roman_numbers/from_roman/Approach2_tests.cs
@@ -18,6 +18,7 @@ namespace from_roman
         
         [Theory]
         [InlineData("I", new[] {1})]
+        [InlineData("V", new[] {5})]
         public void Parse_tests(string roman, int[] expectedValues)
         {
             var result = Parse(roman);
@@ -33,7 +34,20 @@ namespace from_roman
 
         private IEnumerable<int> Parse(string roman)
         {
-            return new[] {1};
+            return new[] {MapDigit(roman[0])};
+            
+            int MapDigit(char digit) {
+                switch (digit) {
+                    case 'I': return 1;
+                    case 'V' : return 5;
+                    case 'X': return 10;
+                    case 'L': return 50;
+                    case 'C': return 100;
+                    case 'D': return 500;
+                    case 'M': return 1000;
+                    default: throw new InvalidOperationException();
+                }
+            }
         }
 
         private IEnumerable<int> AdjustForSubtractionRule(IEnumerable<int> values)
