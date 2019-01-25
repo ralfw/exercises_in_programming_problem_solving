c1c4ef4ccd7091a0592a9862f6d504e599189cc8
Ralf Westphal <info@ralfw.de>
2019-01-25 10:45:53 +0100

    t second acceptance test

--- sudoku/sudoku/tests/Acceptance_tests.cs
+++ sudoku/sudoku/tests/Acceptance_tests.cs
@@ -9,10 +9,33 @@ namespace sudoku
         public void Puzzle2()
         {
             var puzzle = new[,] {
-                {0, 0, 3, 0},
-                {3, 0, 0, 2},
-                {0, 1, 0, 0},
-                {4, 0, 0, 1}
+                {0,0, 3,0},
+                {3,0, 0,2},
+                
+                {0,1, 0,0},
+                {4,0, 0,1}
+            };
+
+            var result = SudokuSolver.Solve(puzzle);
+            
+            Assert.True(SolutionChecker.Check(result));
+        }
+        
+        [Fact]
+        public void Puzzle3()
+        {
+            var puzzle = new[,] {
+                {0,0,0, 2,6,0, 7,0,1},
+                {6,8,0, 0,7,0, 0,9,0},
+                {1,9,0, 0,0,4, 5,0,0},
+                
+                {8,2,0, 1,0,0, 0,4,0},
+                {0,0,4, 6,0,2, 9,0,0},
+                {0,5,0, 0,0,3, 0,2,8},
+                
+                {0,0,9, 3,0,0, 0,7,4},
+                {0,4,0, 0,5,0, 0,3,6},
+                {7,0,3, 0,1,8, 0,0,0}
             };
 
             var result = SudokuSolver.Solve(puzzle);
