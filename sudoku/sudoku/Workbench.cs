d7894b3786b8e551d748bb8b351b0c1e20b3e557
Ralf Westphal <info@ralfw.de>
2019-01-30 09:38:16 +0100

    Implemented Workbench.Cell

--- sudoku/sudoku/Workbench.cs
+++ sudoku/sudoku/Workbench.cs
@@ -1,17 +1,47 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using Xunit;
 
 namespace sudoku
 {
+    public class Workbench_test
+    {
+        [Fact]
+        public void Cell() {
+            var sut = new Workbench.Cell(3);
+            
+            Assert.False(sut.IsFixed);
+            
+            sut.RemoveCandidate(3);
+            Assert.False(sut.IsFixed);
+            
+            sut.RemoveCandidate(2);
+            Assert.True(sut.IsFixed);
+            Assert.Equal(1, sut.SolutionNumber);
+            
+            sut.RemoveCandidate(3);
+            Assert.True(sut.IsFixed);
+            
+            sut.RemoveCandidate(1);
+            Assert.False(sut.IsFixed);
+        }
+    }
+    
+    
     class Workbench
     {
         public class Cell {
-            private List<int> _candidateNumbers;
-            
-            public bool IsFixed { get; }
-            public int SolutionNumber { get; }
-            
-            public void RemoveCandidate(int number) {}
+            private readonly List<int> _candidateNumbers;
+
+            public Cell(int n) {
+                _candidateNumbers = new List<int>(Enumerable.Range(1,n));
+            }
+
+            public bool IsFixed => _candidateNumbers.Count == 1;
+            public int SolutionNumber => _candidateNumbers.First();
+
+            public void RemoveCandidate(int number) => _candidateNumbers.Remove(number);
         }
     
         
